f57a5efae483067846610481da63f393badac829
John Beech <johnbeech.github@mkv25.net>
2024-01-02 21:10:42 +0000

    Refactor Pixi coded into dedicated TS file

---
+++ src/canvas/Game.ts
@@ -0,0 +1,74 @@
+import * as PIXI from 'pixi.js'
+
+export default class Game {
+  $canvas: HTMLElement
+  app: PIXI.Application
+  camera: PIXI.Container
+
+  constructor($canvas:HTMLElement) {
+    this.$canvas = $canvas
+    
+    const app = new PIXI.Application({
+      width: 512,
+      height: 512,
+      antialias: false,
+      backgroundColor: 0x1099bb,
+      resolution: 1
+    })
+
+    const camera = new PIXI.Container()
+    this.app = app
+    this.camera = camera
+
+    this.setup(app, camera)
+  }
+
+  private setup(app: PIXI.Application, camera: PIXI.Container) {
+    const $canvas = this.$canvas
+    const appView = app.view as any
+    $canvas.appendChild(appView)
+    app.renderer.resize($canvas.clientWidth, $canvas.clientHeight)
+
+
+    const container = new PIXI.Container()
+    const texture = PIXI.Texture.from('./assets/shuttle.png')
+
+    const shuttle = new PIXI.Sprite(texture)
+    shuttle.anchor.set(0.5)
+    shuttle.width = 256
+    shuttle.height = 256
+    container.addChild(shuttle)
+
+    camera.x = (app.screen.width) / 2
+    camera.y = (app.screen.height) / 2
+
+    camera.addChild(container)
+    app.stage.addChild(camera)
+
+    app.ticker.add(() => {
+      camera.rotation -= 0.01;
+    })
+
+    window.onresize = () => {
+      this.resizeCanvas()
+    }
+
+    this.app = app
+    this.camera = camera
+  }
+
+  resizeCanvas() {
+    const { app, camera } = this
+    const $canvas = this.$canvas as HTMLElement
+    app?.renderer.resize($canvas.clientWidth, $canvas.clientHeight)
+
+    if (camera && app) {
+      camera.x = (app.screen.width) / 2
+      camera.y = (app.screen.height) / 2
+    }
+  }
+
+  destroy() {
+    this.app.destroy()
+  }
+}
